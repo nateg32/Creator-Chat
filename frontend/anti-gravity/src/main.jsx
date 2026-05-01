3f92c0784bb3035d5838bb9e84ec803d88ce9bab
nateg32 <s4169571@student.rmit.edu.au>
2026-05-01 11:32:03 +1000

    refactor: Remove cosy premium theme stylesheet to streamline UI

--- frontend/anti-gravity/src/main.jsx
+++ frontend/anti-gravity/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './theme-cosy.css'
 import App from './App.jsx'
 import { FeedbackProvider } from './components/feedback/FeedbackProvider.jsx'
 
